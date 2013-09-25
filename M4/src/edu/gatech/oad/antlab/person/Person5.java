54ae35a43479c6a320736db3754d739522c1ec62
mvirgil3@gatech.edu <mvirgil3@gatech.edu@5ec24329-c111-5b0a-d3f2-46aa1496f386>
2013-09-25 14:19:58 +0000

    Person5 updated

--- M4/src/edu/gatech/oad/antlab/person/Person5.java
+++ M4/src/edu/gatech/oad/antlab/person/Person5.java
@@ -30,8 +30,9 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+	  String firstPart = input.substring(0, 2);
+	  String secondPart = input.substring(3);
+	  return secondPart + firstPart;
 	}
 	
 	/**
