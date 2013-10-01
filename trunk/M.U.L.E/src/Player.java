12bd26eb23717afbde8fe38744d8cc7f6c4332ad
k2cipolla@gmail.com <k2cipolla@gmail.com@5ec24329-c111-5b0a-d3f2-46aa1496f386>
2013-10-01 01:58:39 +0000

    Player class added

---
+++ trunk/M.U.L.E/src/Player.java
@@ -0,0 +1,28 @@
+
+public class Player extends Trader {
+	private String name;
+	private RaceType Race;
+	//private color color;
+	//private EnumMap<InputType, boolean> keyStates;
+	
+	public int calculateScore() {
+		// TODO
+		return 0; 
+	}
+	
+	@Override
+	public void buyMuleFromSeller(Trader seller, Resource muleConfig, int price)
+			throws FailedTransactionException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void buyLandFromSeller(Trader seller, int price)
+			throws FailedTransactionException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
