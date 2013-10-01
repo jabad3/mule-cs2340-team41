f9bb4bfd93001b0ac377ae0d9623ded1ca889ec5
mvirgil3@gatech.edu <mvirgil3@gatech.edu@5ec24329-c111-5b0a-d3f2-46aa1496f386>
2013-10-01 01:49:38 +0000

    Added sutbbed-out Resource, Store, Trader, Inventory, FailedTransactionException files

---
+++ trunk/M.U.L.E/src/Inventory.java
@@ -0,0 +1,19 @@
+/**
+ * The inventory class keeps track of the number of resources it currently has.
+ * 
+ * @author Max
+ *
+ */
+public class Inventory {
+
+    public void removeResource(Resource rType) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void addResource(Resource rType) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
---
+++ trunk/M.U.L.E/src/Store.java
@@ -0,0 +1,26 @@
+/**
+ * The store is responsible for selling mules inside the town, selling land
+ * during land auctions, 
+ * 
+ * The store can also play the role of a buyer buy purchasing land when
+ * 
+ * @author Max
+ *
+ */
+public class Store extends Trader {
+
+    @Override
+    public void buyMuleFromSeller(Trader seller, Resource muleConfig, int price)
+            throws FailedTransactionException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void buyLandFromSeller(Trader seller, int price)
+            throws FailedTransactionException {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
