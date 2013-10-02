39259c6ef568ed786edd52dddaec356937df80a2
mvirgil3@gatech.edu <mvirgil3@gatech.edu@5ec24329-c111-5b0a-d3f2-46aa1496f386>
2013-10-02 00:22:00 +0000

    Setup these constructors (plus default char-map) -- may need to alter depending on final Setup implementation

--- trunk/M.U.L.E/src/Store.java
+++ trunk/M.U.L.E/src/Store.java
@@ -9,6 +9,32 @@
  */
 public class Store extends Trader {
 
+    
+    /**
+     * Creates a store based on its intial quantities of each resource.
+     * 
+     * @param food Initial food quantity
+     * @param energy Initial energy quantity
+     * @param ore Initial ore quantity
+     * @param money Initial money quantity
+     * @param mules Initial mule quantity
+     */
+    public Store(int food, int energy, int ore, int money, int mules) {
+        super(food, energy, ore, money, mules);
+    }
+    
+    /**
+     * Creates a store based on a difficulty.
+     * 
+     * @param difficulty
+     */
+    public Store(Difficulty difficulty) {
+        if (difficulty == Difficulty.BEGINNER)
+            inventory = new Inventory(16, 16, 0, Integer.MAX_VALUE / 2, 0);
+        else  // Standard or Tournament
+            inventory = new Inventory(8, 8, 8, Integer.MAX_VALUE / 2, 8);
+    }
+    
     @Override
     public void buyMuleFromSeller(Trader seller, Resource muleConfig, int price)
             throws FailedTransactionException {
@@ -21,6 +47,10 @@ public class Store extends Trader {
             throws FailedTransactionException {
         // TODO Auto-generated method stub
         
+    }
+    
+    public void buildMulesWithOre() {
+        // TODO
     }
 
 }
