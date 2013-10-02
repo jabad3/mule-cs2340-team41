1db85b08f86a5036b343683d5aff064b25549382
k2cipolla@gmail.com <k2cipolla@gmail.com@5ec24329-c111-5b0a-d3f2-46aa1496f386>
2013-10-02 23:53:43 +0000

    Basic outline for GameModel class.  As I said in the email, I'm unsure of whether we want to handle sorting players within the GameModel or not.

---
+++ M.U.L.E/src/GameModel.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class GameModel {
+	private List<Player> playerList = new ArrayList<>();
+	private Difficulty difficulty;
+	private Store store;
+	//private GameMap gameMap;
+	
+	public GameModel() {
+		// TODO
+	}
+	
+	/*
+	 * adds a player to the playerList
+	 * @param player
+	 */
+	public void addPlayer(Player player) {
+		playerList.add(player);
+	}
+	
+	/*
+	 * sets the GameModel's difficulty
+	 * @param difficulty
+	 */
+	public void setDifficulty(Difficulty difficulty) {
+		this.difficulty = difficulty;
+	}
+	
+	/*
+	 * adds a store to GameModel
+	 * @param store
+	 */
+	public void addStore(Store store) {
+			this.store = store;
+	}
+	
+	/*private void sortPlayerList() {
+		//sorts the list of players
+	}
+										//undecided on whether to have this functionality here
+	public void getPlayerOrder() {
+		sortPlayerList();
+		//return playerList
+	}
+	
+	*/
+	
+	
+}
