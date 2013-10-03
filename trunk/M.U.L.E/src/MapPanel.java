942803355babd1118bab9e6d2e96303fd7642a7c
thumbsupep <thumbsupep@5ec24329-c111-5b0a-d3f2-46aa1496f386>
2013-10-03 06:51:00 +0000

    added a MapGenerator class and created a character map in the MapPanel

--- trunk/M.U.L.E/src/MapPanel.java
+++ trunk/M.U.L.E/src/MapPanel.java
@@ -5,6 +5,10 @@ public class MapPanel extends JPanel{
 
 	public MapPanel()
 	{
+		char[][] CharMap = MapGenerator.generateMap('A','B','C','P','R','T');
+		//a is one mountain, b is two, c is three
 		
+		//map = new LandPlot[CharMap.length][CharMap[0].length];
+		// ^ a map of landplots (what class are we using for that?) as opposed to characters
 	}
 }
