db09558623319ef20e5e7e2457b68dc6fc65b8fc
jabadjka3 <jabadjka3@5ec24329-c111-5b0a-d3f2-46aa1496f386>
2013-10-04 20:26:47 +0000

    revised

--- M.U.L.E/src/PlayerConfigStage.java
+++ M.U.L.E/src/PlayerConfigStage.java
@@ -18,14 +18,14 @@ public class PlayerConfigStage {
 	}
 	}
 
-    private class FinishedListener implements ActionListener {
-        
-        /**
-         * When the View is "finished", set up the model accordingly,
-         * then terminate this stage of the game.
-         * @param e
-         */
-        public void actionPerformed(ActionEvent e) {
-        	PlayerConfigStage.this.configureModel();
-            PlayerConfigStage.this.endStage();
-}
+//    private class FinishedListener implements ActionListener {
+//        
+//        /**
+//         * When the View is "finished", set up the model accordingly,
+//         * then terminate this stage of the game.
+//         * @param e
+//         */
+//        public void actionPerformed(ActionEvent e) {
+//        	PlayerConfigStage.this.configureModel();
+//            PlayerConfigStage.this.endStage();
+//}
