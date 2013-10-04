3ef65936ea468c163956eb82a80deffab2c98a76
jabadjka3 <jabadjka3@5ec24329-c111-5b0a-d3f2-46aa1496f386>
2013-10-04 20:15:09 +0000

    added quick action listener, not working

--- trunk/M.U.L.E/src/PlayerConfigStage.java
+++ trunk/M.U.L.E/src/PlayerConfigStage.java
@@ -1,3 +1,6 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * 
  */
@@ -8,4 +11,14 @@
  */
 public class PlayerConfigStage {
 
+    private class FinishedListener implements ActionListener {
+        
+        /**
+         * When the View is "finished", set up the model accordingly,
+         * then terminate this stage of the game.
+         * @param e
+         */
+        public void actionPerformed(ActionEvent e) {
+        	PlayerConfigStage.this.configureModel();
+            PlayerConfigStage.this.endStage();
 }
