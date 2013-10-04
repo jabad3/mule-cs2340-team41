f17682785397e9865b8e4ab50479366df398673d
k2cipolla@gmail.com <k2cipolla@gmail.com@5ec24329-c111-5b0a-d3f2-46aa1496f386>
2013-10-04 16:22:52 +0000

    added fields and getters to LandPlotType enum

--- trunk/M.U.L.E/src/LandPlotType.java
+++ trunk/M.U.L.E/src/LandPlotType.java
@@ -1,4 +1,38 @@
+/*
+ * LandPlotType enum type, each type has a foodRate, energyRate, and oreRate
+ * @author Kevin
+ */
+
 
 public enum LandPlotType {
-	RIVER, PLAIN, MTN_1, MTN_2, MTN_3, TOWN
+	RIVER(4, 2, 0), 
+	PLAIN(2, 3, 1),
+	MTN_1(1, 1, 2),
+	MTN_2(1, 1, 3),
+	MTN_3(1, 1, 4),
+	TOWN(0, 0, 0);
+	
+	private final int foodRate;
+	private final int energyRate;
+	private final int oreRate;
+	
+	private LandPlotType(int foodRate, int energyRate, int oreRate) {
+		this.foodRate = foodRate;
+		this.energyRate = energyRate;
+		this.oreRate = oreRate;
+	}
+	
+	public int foodRate() {
+		return foodRate;
+	}
+	
+	public int energyRate() {
+		return energyRate;
+	}
+	
+	public int oreRate() {
+		return oreRate;
+	}
+	
+	
 }
