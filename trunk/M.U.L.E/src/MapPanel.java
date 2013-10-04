a47eb4383c7f91f3f5dc5bbed240885289d3972e
mvirgil3@gatech.edu <mvirgil3@gatech.edu@5ec24329-c111-5b0a-d3f2-46aa1496f386>
2013-10-04 22:13:41 +0000

    Player constructor, RaceType enum w/ images, MapFactory changes

--- trunk/M.U.L.E/src/MapPanel.java
+++ trunk/M.U.L.E/src/MapPanel.java
@@ -5,7 +5,7 @@ public class MapPanel extends JPanel{
 
 	public MapPanel()
 	{
-		char[][] charMap = MapGenerator.generateDefaultMap('A','B','C','P','R','T');
+		//char[][] charMap = MapGenerator.generateDefaultMap('A','B','C','P','R','T');
 		//a is one mountain, b is two, c is three
 		
 		//map = new LandPlot[CharMap.length][CharMap[0].length];
