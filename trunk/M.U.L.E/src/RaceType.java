d81dafa2f2f54ead598ad3fe6fe8bae7118cf032
k2cipolla@gmail.com <k2cipolla@gmail.com@5ec24329-c111-5b0a-d3f2-46aa1496f386>
2013-10-04 01:04:24 +0000

    new version of RaceType

--- trunk/M.U.L.E/src/RaceType.java
+++ trunk/M.U.L.E/src/RaceType.java
@@ -1,4 +1,19 @@
 
 public enum RaceType {
-	HUMAN, FLAPPER, BONZOID, UGAITE, BUZZITE
+	HUMAN(600), 
+	FLAPPER(1600), 
+	BONZOID(1000), 
+	UGAITE(1000), 
+	BUZZITE(1000);
+	
+	private final int startingMoney;
+	
+	private RaceType(int startingMoney) {
+		this.startingMoney = startingMoney;
+	}
+	
+	public int startingMoney() {
+		return startingMoney;
+	}
+	
 }
