54a6464594f025b01cd9f88296b5d05a5913610a
mvirgil3@gatech.edu <mvirgil3@gatech.edu@5ec24329-c111-5b0a-d3f2-46aa1496f386>
2013-10-08 02:02:17 +0000

    LandSelectionView (unfinished)

---
+++ M.U.L.E/src/LandSelectionView.java
@@ -0,0 +1,28 @@
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * The View used during the Land Selection Stage of the game.
+ * @author Max
+ *
+ */
+public class LandSelectionView extends JPanel {
+    
+    JLabel playerNameLabel;
+    JLabel landPlotPriceLabel;
+    JPanel mapPanel;
+    
+    public LandSelectionView(MapPanel mapPanel, int landPlotPrice, String firstPlayerName) {
+        playerNameLabel = new JLabel(firstPlayerName);
+        landPlotPriceLabel = new JLabel("" + landPlotPrice);
+        this.mapPanel = mapPanel;
+        
+        // TODO
+        // add stuff to the View's main panel to get desired display
+    }
+
+    public void changeCurrentPlayerName(String playerName) {
+        playerNameLabel.setText(playerName);
+    }
+
+}
