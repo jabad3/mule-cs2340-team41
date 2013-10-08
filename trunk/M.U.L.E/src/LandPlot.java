2fe443f8a497fa5e90d0069d41b4f2dbf2b6030b
k2cipolla@gmail.com <k2cipolla@gmail.com@5ec24329-c111-5b0a-d3f2-46aa1496f386>
2013-10-08 02:21:17 +0000

    added player field.  Added getter and setter for player (owner)

--- trunk/M.U.L.E/src/LandPlot.java
+++ trunk/M.U.L.E/src/LandPlot.java
@@ -1,10 +1,19 @@
 
 public class LandPlot {
-
+	
+	private Player player;
     private LandPlotType landType;
     
     public LandPlot(LandPlotType landType) {
         this.landType = landType;
+    }
+    
+    public Player getOwner() {
+    	return player;
+    }
+    
+    public void setOwner(Player player) {
+    	this.player = player;
     }
     
     /* for temporary "testing" */
