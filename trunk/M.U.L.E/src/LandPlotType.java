dff3d406247c5bd99ffc650403528e8984e2a4f2
mvirgil3@gatech.edu <mvirgil3@gatech.edu@5ec24329-c111-5b0a-d3f2-46aa1496f386>
2013-10-09 23:43:17 +0000

    Mooore javadocs, plus removed unused code in NamePanel.

--- trunk/M.U.L.E/src/LandPlotType.java
+++ trunk/M.U.L.E/src/LandPlotType.java
@@ -1,11 +1,11 @@
 import javax.swing.ImageIcon;
 
-/*
- * LandPlotType enum type, each type has a foodRate, energyRate, and oreRate
+/**
+ * LandPlotType enum type.
+ * Each LandPlotType has a foodRate, energyRate, and oreRate
+ * 
  * @author Kevin
  */
-
-
 public enum LandPlotType {
 	RIVER(4, 2, 0, "river.png"), 
 	PLAIN(2, 3, 1, "plain.png"),
@@ -14,11 +14,26 @@ public enum LandPlotType {
 	MTN_3(1, 1, 4, "m3.png"),
 	TOWN(0, 0, 0, "town.png");
 	
+	/** Base food production for the land plot type */
 	private final int foodRate;
+	
+	/** Base energy production for the land plot type */
 	private final int energyRate;
+	
+	/** Base ore production for the land plot type */
 	private final int oreRate;
+	
+	/** File name for the stock image file associated with the land plot type */
 	private String iconFileName;
 	
+	/**
+	 * Assign appropriate production rates for all land plot types
+	 * 
+	 * @param foodRate Food production rate
+	 * @param energyRate Energy production rate
+	 * @param oreRate Ore production rate
+	 * @param iconFileName Stock image file name
+	 */
 	private LandPlotType(int foodRate, int energyRate, int oreRate, String iconFileName) {
 		this.foodRate = foodRate;
 		this.energyRate = energyRate;
@@ -27,18 +42,38 @@ public enum LandPlotType {
 		
 	}
 	
+	/**
+	 * Get the food production rate for the land plot type.
+	 * 
+	 * @return Food production rate
+	 */
 	public int foodRate() {
 		return foodRate;
 	}
 	
+	/**
+	 * Get the energy production rate for the land plot type.
+	 * 
+	 * @return Energy production rate
+	 */
 	public int energyRate() {
 		return energyRate;
 	}
 	
+	/**
+	 * Get the ore production rate for the land plot type.
+	 * 
+	 * @return Ore production rate
+	 */
 	public int oreRate() {
 		return oreRate;
 	}
 	
+	/**
+	 * Get the ImageIcon object associated with the land plot type.
+	 * 
+	 * @return ImageIcon for the land plot type
+	 */
 	public ImageIcon getStockImageIcon() {
 		return new ImageIcon(iconFileName);
 	}
