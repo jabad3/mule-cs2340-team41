8119235cd86d52e113ab2420fa23b9c99e970dce
mvirgil3@gatech.edu <mvirgil3@gatech.edu@5ec24329-c111-5b0a-d3f2-46aa1496f386>
2013-10-11 05:51:50 +0000

    Minor update:  Some javadocs, as well as some stubbed-out methods.

--- M.U.L.E/src/LandSelectionView.java
+++ M.U.L.E/src/LandSelectionView.java
@@ -5,15 +5,35 @@ import javax.swing.JPanel;
 
 /**
  * The View used during the Land Selection Stage of the game.
+ * The LandSelectionView is responsible for displaying the current Player's
+ * name, the current price of the land, and the map.
+ * 
+ * Interaction with the View includes selecting a land plot to purchase or
+ * choosing the option to pass on selecting a land plot.
+ * 
  * @author Max
  *
  */
 public class LandSelectionView extends JPanel {
     
+    /** Label to display the current Player's name. */
     JLabel playerNameLabel;
-    JLabel landPlotPriceLabel;
-    JPanel mapPanel;
     
+    /** Label to display the current price of land. */
+    JLabel landPlotPriceLabel;
+    
+    /** Displays the land plots to the user. */
+    MapPanel mapPanel;
+    
+    /**
+     * Creates a LandSelectionView using an already-instantiated MapPanel
+     * object.  Sets the layout of the View and adds appropriate components to
+     * the View.
+     * 
+     * @param mapPanel The MapPanel to be displayed
+     * @param landPlotPrice The current price of land
+     * @param firstPlayerName The name of the current player
+     */
     public LandSelectionView(MapPanel mapPanel, int landPlotPrice, String firstPlayerName) {
         playerNameLabel = new JLabel(firstPlayerName);
         landPlotPriceLabel = new JLabel("" + landPlotPrice);
@@ -26,6 +46,11 @@ public class LandSelectionView extends JPanel {
         this.add(playerNameLabel, BorderLayout.SOUTH);
     }
 
+    /**
+     * Set the current Player's name
+     * 
+     * @param playerName The new name to display
+     */
     public void setCurrentPlayerName(String playerName) {
         playerNameLabel.setText(playerName);
     }
