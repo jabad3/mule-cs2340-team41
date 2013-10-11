4328af5601fa5809146ea3b93a865f5d28c1695f
jabadjka3 <jabadjka3@5ec24329-c111-5b0a-d3f2-46aa1496f386>
2013-10-11 14:58:34 +0000

    removing my bugs and isolating development stage

--- trunk/M.U.L.E/src/DevelopmentStageView.java
+++ trunk/M.U.L.E/src/DevelopmentStageView.java
@@ -1,3 +1,6 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -9,5 +12,33 @@ import javax.swing.JPanel;
  *
  */
 public class DevelopmentStageView extends JPanel {
+    
+    /** Label to display the current Player's name. */
+    JLabel playerNameLabel;
+    
+    /** Displays the land plots to the user. */
+    MapPanel mapPanel;
+    
+    public DevelopmentStageView(MapPanel mapPanel, String firstPlayerName) {
+        playerNameLabel = new JLabel(firstPlayerName);
+        this.mapPanel = mapPanel;
+        
+        // TODO
+        this.setLayout(new BorderLayout());
+        this.add(mapPanel, BorderLayout.CENTER);
+        this.add(playerNameLabel, BorderLayout.SOUTH);
+    }
 
+    /**
+     * Set the current Player's name
+     * 
+     * @param playerName The new name to display
+     */
+    public void setCurrentPlayerName(String playerName) {
+        playerNameLabel.setText(playerName);
+    }
+ 
+    
+    
 }
+
