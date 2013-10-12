f09b890299664eefd267d3b19dac7532611c91ab
jabadjka3 <jabadjka3@5ec24329-c111-5b0a-d3f2-46aa1496f386>
2013-10-12 21:19:57 +0000

    this is a neat looking key listener. i integrated it into our devStage locally but its not working yet.

---
+++ trunk/M.U.L.E/src/Views/MyPanel.java
@@ -0,0 +1,30 @@
+package Views;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MyPanel extends JPanel implements KeyListener {
+    private char temp = 't';
+    public MyPanel() {
+        this.setPreferredSize(new Dimension(100, 100));
+        addKeyListener(this);}
+    public void addNotify() {
+        super.addNotify();
+        requestFocus();}
+    public void paintComponent(Graphics g) {
+        g.clearRect(0, 0, getWidth(), getHeight());
+        g.drawString(""+temp, 50, 50);}
+    public void keyPressed(KeyEvent e) { }
+    public void keyReleased(KeyEvent e) { }
+    public void keyTyped(KeyEvent e) {
+        temp = e.getKeyChar();
+        repaint();}
+    public static void main(String[] s) {
+        JFrame f = new JFrame();
+        f.getContentPane().add(new MyPanel());
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.pack();
+        f.setVisible(true);}}
