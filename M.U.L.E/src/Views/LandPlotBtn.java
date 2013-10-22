40e9b3ea060037df13c896e5d2d43e475a2caaf6
mvirgil3@gatech.edu <mvirgil3@gatech.edu@5ec24329-c111-5b0a-d3f2-46aa1496f386>
2013-10-22 16:23:04 +0000

    javadoc

--- M.U.L.E/src/Views/LandPlotBtn.java
+++ M.U.L.E/src/Views/LandPlotBtn.java
@@ -6,10 +6,29 @@ import java.awt.event.MouseListener;
 import javax.swing.BorderFactory;
 import Models.LandPlot;
 
+/**
+ * LandPlotBtn represents the visual representation of a land plot in the
+ * game model.
+ * 
+ * LandPlotBtns are icons that display the type of land and the owner of the
+ * land.  Ownership is signified by coloring the border to the owner's color.
+ * Unowned land plots should have no border.
+ * 
+ * @author Max
+ *
+ */
 public class LandPlotBtn extends ResizableIcon {
     
+    /** The land plot model object associated with this icon. */
     private LandPlot myLandPlot;
 
+    /**
+     * Create a LandPlotBtn object.
+     * 
+     * @param landPlot The land plot model object to be displayed
+     * @param buttonListener A mouse listener for the land plot object that is
+     * used to respond to user interaction with the land plot icon
+     */
     public LandPlotBtn(LandPlot landPlot, MouseListener buttonListener) {
         super(landPlot.getLandType().getStockImageIcon());
         this.myLandPlot = landPlot;
@@ -23,6 +42,9 @@ public class LandPlotBtn extends ResizableIcon {
         // TODO needed later for painting mule, resource
     }
 
+    /**
+     * Draw the correct border by querying the land plot model object.
+     */
     private void drawBorder() {
         if (myLandPlot.getOwner() == null) {
             setBorderToDefault();
@@ -32,10 +54,18 @@ public class LandPlotBtn extends ResizableIcon {
         }
     }
     
+    /**
+     * Get the land plot model object associated with this icon.
+     * 
+     * @return The LandPlot for this icon
+     */
     public LandPlot getMyLandPlot() {
         return myLandPlot;
     }
 
+    /**
+     * Set the border to the default empty border.
+     */
     public void setBorderToDefault() {
         this.setBorder(BorderFactory.createEmptyBorder());
     }
