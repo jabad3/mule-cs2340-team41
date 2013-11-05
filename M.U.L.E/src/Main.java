de16dfef62f2e084b83322f6a15d9c71c7e3b0e7
jabadjka3 <jabadjka3@5ec24329-c111-5b0a-d3f2-46aa1496f386>
2013-11-05 00:02:02 +0000

    a view to let users load games or start anew

--- M.U.L.E/src/Main.java
+++ M.U.L.E/src/Main.java
@@ -1,4 +1,9 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import Models.Game;
 /**
@@ -8,22 +13,68 @@ import Models.Game;
  * @version 1
  */
 
-public class Main
-{
+public class Main {
     /**
      * Creates the main JFrame container for MULE, then runs the Game
      * object.
      */
-	public static void main(String[] args)
-	{
+	
+	public Main() {
 		JFrame frame = new JFrame("M.U.L.E");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		Game curGame = new Game(frame);
-		curGame.start();
+		JPanel panel = new JPanel();
+		JButton button = new JButton("Load");
+		JButton button2 = new JButton("New Game");
+		ButtonListener listener = new ButtonListener(button, button2, frame);
+		
+		button.addActionListener(listener);
+		button2.addActionListener(listener);
+		
+		panel.add(button);
+		panel.add(button2);
+		frame.add(panel);
 		
 		frame.pack();
 		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+	
+	public class ButtonListener implements ActionListener {
+		JButton button1;
+		JButton button2;
+		JFrame frame;
+		
+		public ButtonListener(JButton button1, JButton button2, JFrame frame) {
+			// TODO Auto-generated constructor stub
+			this.button1 = button1;
+			this.button2 = button2;
+			this.frame = frame;
+		}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == button1) {
+				System.out.println("Load");
+				//open a load file dialog
+				//call new constructor
+				//Game curGame = new Game(frame, gameModel);
+				//curGame.start();
+			}
+			if (e.getSource() == button2) {
+				System.out.println("New Game");
+				Game curGame = new Game(frame);
+				curGame.start();
+			}
+		}
+		
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		Main m = new Main();
+		//Game curGame = new Game(frame);
+		//curGame.start();
 	}
 
 }
