3602731e891681a1ef565f34c07aa19ef2041c27
jabadjka3 <jabadjka3@5ec24329-c111-5b0a-d3f2-46aa1496f386>
2013-11-07 23:54:10 +0000

    factored out main. key listeners work now

--- trunk/M.U.L.E/src/Main.java
+++ trunk/M.U.L.E/src/Main.java
@@ -7,6 +7,7 @@ import javax.swing.JPanel;
 
 import Models.Game;
 import Models.GameModel;
+import Stages.StartScreen;
 /**
  * This class executes the game of MULE.
  * 
@@ -19,65 +20,10 @@ public class Main {
      * Creates the main JFrame container for MULE, then runs the Game
      * object.
      */
-	
-	public Main() {
-		JFrame frame = new JFrame("M.U.L.E");
 		
-		JPanel panel = new JPanel();
-		JButton button = new JButton("Load");
-		JButton button2 = new JButton("New Game");
-		ButtonListener listener = new ButtonListener(button, button2, frame);
-		
-		button.addActionListener(listener);
-		button2.addActionListener(listener);
-		
-		panel.add(button);
-		panel.add(button2);
-		frame.add(panel);
-		
-		frame.pack();
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-	}
-	
-	public class ButtonListener implements ActionListener {
-		JButton button1;
-		JButton button2;
-		JFrame frame;
-		
-		public ButtonListener(JButton button1, JButton button2, JFrame frame) {
-			// TODO Auto-generated constructor stub
-			this.button1 = button1;
-			this.button2 = button2;
-			this.frame = frame;
-		}
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			if (e.getSource() == button1) {
-				System.out.println("Loading");
-				
-				GameModel saved = GameModel.loadGame();
-				
-				//call new constructor
-				Game curGame = new Game(frame, saved);
-				curGame.start();
-			}
-			if (e.getSource() == button2) {
-				System.out.println("New Game");
-				Game curGame = new Game(frame);
-				curGame.start();
-			}
-		}
-		
-	}
-	
-	
 	public static void main(String[] args)
 	{
-		Main m = new Main();
-		//Game curGame = new Game(frame);
-		//curGame.start();
+		StartScreen startscreen = new StartScreen();
 	}
 
 }
