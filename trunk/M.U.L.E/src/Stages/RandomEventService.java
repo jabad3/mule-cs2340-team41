d03d88db2e5c00b346942001e6195a282918d453
k2cipolla@gmail.com <k2cipolla@gmail.com@5ec24329-c111-5b0a-d3f2-46aa1496f386>
2013-11-08 21:56:51 +0000

    added buildmValues map to RandomEventService.  Added constructor.  RandomEventService should now function.

--- trunk/M.U.L.E/src/Stages/RandomEventService.java
+++ trunk/M.U.L.E/src/Stages/RandomEventService.java
@@ -2,17 +2,34 @@ package Stages;
 import Models.Player;
 import RandomEvents.RandomEvent;
 import RandomEvents.RandomEventFactory;
+import Models.GameModel;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
 public class RandomEventService {
 	private RandomEventFactory factory = new RandomEventFactory();
 	private Player player;
-	//private GameModel gameModel;
-	//private List<Player> playerList;
 	Random rand = new Random();
+	HashMap<Integer, Integer> mValues;
+	private GameModel gameModel;
 	
+	/**
+	 * constructor for RandomEventService, builds mValues map, 
+	 * and instantiates GameModel
+	 */
+	public RandomEventService() {
+		this.buildmValues();
+		gameModel = new GameModel();
+	}
+	
+	/**
+	 * determines if an event is going to occur.  There is a 27%
+	 * chance of an event occuring.
+	 * 
+	 * @return true if an event is to occur, false otherwise
+	 */
 	private boolean eventIsHappening() {
 		if(rand.nextInt(101) <= 27)
 			return true;
@@ -20,12 +37,36 @@ public class RandomEventService {
 			return false;
 	}
 	
+	/** 
+	 * Build a Map with mvalues (values) for each round (keys). 
+	 */
+	private void buildmValues() {
+		mValues = new HashMap<>();
+		mValues.put(1, 25);
+		mValues.put(2, 25);
+		mValues.put(3, 25);
+		mValues.put(4, 50);
+		mValues.put(5, 50);
+		mValues.put(6, 50);
+		mValues.put(7, 50);
+		mValues.put(8, 75);
+		mValues.put(9, 75);
+		mValues.put(10, 75);
+		mValues.put(11, 75);
+		mValues.put(12, 100);
+	}
+	
+	/**
+	 * Starts RandomEventService for given player, will 
+	 * determine if an event is going to occur and then execute appropriate
+	 * type of event
+	 * 
+	 * @param player the player to run service on
+	 * @param isInLastPlace whether the player is in last place or not
+	 */
 	public void startService(Player player, boolean isInLastPlace) { 
 		this.player = player;
-		
-		/// PLACE HOLDER!!!!!!!!!!!  GRAB M FROM TABLE!!! ///
-		int m = 500;
-		/// PLACE HOLDER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ///
+		int m = mValues.get(gameModel.getCurrentRound());
 		
 		if(eventIsHappening() && isInLastPlace) {
 			RandomEvent event = factory.buildGoodEvent();
