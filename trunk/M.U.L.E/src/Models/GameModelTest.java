96ac7f28e4e3e55b885f72f1f6d91f51e77b4f24
thumbsupep <thumbsupep@5ec24329-c111-5b0a-d3f2-46aa1496f386>
2013-11-12 23:00:56 +0000

    set up the JUnit file to test CalculateTurnTime(Player player)

---
+++ trunk/M.U.L.E/src/Models/GameModelTest.java
@@ -0,0 +1,18 @@
+package Models;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * These are the tests for the method CalculateTurnTime in the class GameModel
+ *
+ */
+public class GameModelTest {
+
+	@Test
+	public void testCalculateTurnTime() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
